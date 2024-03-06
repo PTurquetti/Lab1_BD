e1614326e4257900010253e360d401a2d4a4c063
Paulo Turquetti <105894229+PHVedo@users.noreply.github.com>
2024-03-06 20:45:30 -0300

    Update P2_Deletes.sql

--- P2_Deletes.sql
+++ P2_Deletes.sql
@@ -2,4 +2,14 @@
 
 -- a)
 DELETE FROM SISTEMA WHERE ESTRELA = '123456';
+/* A tabela sistema nao eh referenciada por nenhuma outra tabela */
 
+
+
+-- b)
+DELETE FROM PLANETA WHERE DESIGNACAO_ASTRONOMICA = 'Vênus';
+/* A tabela Planeta eh referenciada pelas tabelas OrbinaPlaneta, Especie, Habitacao
+e Dominancia. No entanto, a tupla que possui DESIGNACAO_ASTRONOMICA = 'Vênus' nao eh
+referenciada em nenhuma tupla das tabelas citadas anteriormente, configurando o caso
+proposto pelo enunciado
+*/
