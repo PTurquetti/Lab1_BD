ec9fe0dc38e48e8ad4a73f10b662610b96af95f0
Paulo Turquetti <105894229+PHVedo@users.noreply.github.com>
2024-03-06 21:41:13 -0300

    Update P2_Updates.sql

--- P2_Updates.sql
+++ P2_Updates.sql
@@ -5,23 +5,26 @@ UPDATE FEDERACAO SET
     NOME_FD = 'Nova Aliança Galática',
     DATA_FUND = TO_DATE('22/03/3200','dd/mm/yyyy')
 WHERE NOME_FD = 'Aliança Galáctica';
-
-
---INSERT INTO FEDERACAO (NOME_FD, DATA_FUND) VALUES ('Aliança Galáctica', TO_DATE('01/01/2200','dd/mm/yyyy'));
---INSERT INTO FEDERACAO (NOME_FD, DATA_FUND) VALUES ('Comando Estelar', TO_DATE('09-10-2199', 'DD-MM-YYYY'));
-
---SELECT * FROM FEDERACAO;
---DELETE FROM FEDERACAO;
-
+/* Para que o update afete somente uma tupla, a condicao de busca deve estar
+relacionada a chave primaria de uma tabela. Nesse caso, a federacao alterada sera
+a de NOME_FD = 'Aliança Galáctica'. Como NOME_FD eh Chave Primaria, somente essa
+tupla sera afetada */
 
 
 --b)
 UPDATE ESPECIE SET
     EH_INTELIGENTE = 'N'
 WHERE PLANETA_ORIGEM = 'Terra';
+/* Para que o update afete mais de uma tupla, a condicao de busca nao pode estar 
+relacionada a chave primaria da tabela. Nesse caso, todas as especies que possuem
+PLANETA_ORIGEM = 'Terra' terao o atributo EH_INTELIGENTE alterado para 'N' */
+
 
 
 --c)
 UPDATE PLANETA SET
     CLASSIFICACAO = NULL;
+/*Para que o update afete todas as tuplas de uma tabela, nao utilizamos a clausula
+WHERE. Sendo assim, todas as tuplas de PLANETA terao o atributo CLASSIFICACAO alterado
+para NULL */
 
