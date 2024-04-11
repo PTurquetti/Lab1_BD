ab60f56000c5f059f5164ecb1354521a4ed8fce1
Paulo Turquetti <105894229+PTurquetti@users.noreply.github.com>
2024-04-10 21:15:50 -0300

    Update Pratica6_AmandaJoioso_PauloTurquetti.sql

--- Pratica6_AmandaJoioso_PauloTurquetti.sql
+++ Pratica6_AmandaJoioso_PauloTurquetti.sql
@@ -1,1 +1,37 @@
+/* Pratica 6
+
+Amanda Valukas Breviglieri Joioso - 4818232
+Paulo Henrique Vedovatto Turquetti - 13750791
+*/
+
+
+
+-- QUESTÃO 1 --------------------------------------------------------------------------------------------------------
+
+-- a)
+GRANT SELECT ON ESTRELA TO a4818232;
+
+
+
+--b)
+GRANT SELECT ON ESTRELA TO a4818232 WITH GRANT OPTION; -- não executado
+
+
+
+
+
+
+
+-- Removendo o privilégio para as questões a seguir:
+REVOKE SELECT ON ESTRELA FROM a4818232
+
+-- QUESTÃO 2 ---------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
 
