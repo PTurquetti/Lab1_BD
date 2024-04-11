17e81d3e0f45a47f6f76af6e699d525d500ac0e6
Paulo Turquetti <105894229+PTurquetti@users.noreply.github.com>
2024-04-10 21:31:53 -0300

    Update Pratica6_AmandaJoioso_PauloTurquetti.sql

--- Pratica6_AmandaJoioso_PauloTurquetti.sql
+++ Pratica6_AmandaJoioso_PauloTurquetti.sql
@@ -5,11 +5,32 @@ Paulo Henrique Vedovatto Turquetti - 13750791
 */
 
 
+/*
+
+USUÁRIOS:
+
+User 1 - a13750791
+User 2 - a4818232 
+User 3 - Darlam
+
+*/
 
 -- QUESTÃO 1 --------------------------------------------------------------------------------------------------------
 
 -- a)
 GRANT SELECT ON ESTRELA TO a4818232;
+
+-- i. 
+-- Usuário 2 fazendo busca:
+SELECT * FROM a13750791.ESTRELA;
+-- A busca foi realizada com sucesso
+
+-- ii.
+-- User 2 fazendo insercao:
+
+
+
+
 
 
 
