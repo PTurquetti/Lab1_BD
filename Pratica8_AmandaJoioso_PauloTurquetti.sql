cbaaa7ce6d4de83e5ad9e9b33fe109e1ec5a5aae
amandajoioso <142517847+amandajoioso@users.noreply.github.com>
2024-05-06 20:13:11 -0300

    Update Pratica8_AmandaJoioso_PauloTurquetti.sql

--- Pratica8_AmandaJoioso_PauloTurquetti.sql
+++ Pratica8_AmandaJoioso_PauloTurquetti.sql
@@ -24,3 +24,56 @@ JOIN DOMINANCIA D ON D.NACAO = N.NOME;
 
 
 -- QUESTÃO 2 -------------------------------------------------------------------------
+
+DECLARE
+  TYPE t_planeta_info IS TABLE OF PLANETA%ROWTYPE INDEX BY PLS_INTEGER;
+  v_planeta_info t_planeta_info;
+  v_index NUMBER;
+  v_nacao_dominante DOMINANCIA.NACAO%TYPE;
+  v_data_ini DOMINANCIA.DATA_INI%TYPE;
+  v_data_fim DOMINANCIA.DATA_FIM%TYPE;
+  v_qtd_comunidades NUMBER;
+  v_qtd_especies NUMBER;
+  v_qtd_habitantes NUMBER;
+  v_qtd_faccoes NUMBER;
+  v_facao_majoritaria FACCAO.NOME%TYPE;
+  v_qtd_especies_origem NUMBER;
+BEGIN
+  SELECT * BULK COLLECT INTO v_planeta_info FROM PLANETA;
+
+  FOR v_index IN 1..v_planeta_info.COUNT LOOP
+    BEGIN
+      SELECT NACAO, DATA_INI, DATA_FIM INTO v_nacao_dominante, v_data_ini, v_data_fim
+      FROM DOMINANCIA
+      WHERE PLANETA = v_planeta_info(v_index).ID_ASTRO
+      AND DATA_FIM IS NULL;
+
+      SELECT COUNT(*) INTO v_qtd_comunidades
+      FROM COMUNIDADE
+      WHERE ESPECIE IN (SELECT NOME FROM ESPECIE WHERE PLANETA_OR = v_planeta_info(v_index).ID_ASTRO);
+
+      SELECT COUNT(DISTINCT ESPECIE), SUM(QTD_HABITANTES) INTO v_qtd_especies, v_qtd_habitantes
+      FROM COMUNIDADE
+      WHERE ESPECIE IN (SELECT NOME FROM ESPECIE WHERE PLANETA_OR = v_planeta_info(v_index).ID_ASTRO);
+
+      SELECT COUNT(DISTINCT FACCAO) INTO v_qtd_faccoes
+      FROM PARTICIPA
+      WHERE ESPECIE IN (SELECT NOME FROM ESPECIE WHERE PLANETA_OR = v_planeta_info(v_index).ID_ASTRO);
+
+      SELECT FACCAO INTO v_facao_majoritaria
+      FROM PARTICIPA
+      WHERE ESPECIE IN (SELECT NOME FROM ESPECIE WHERE PLANETA_OR = v_planeta_info(v_index).ID_ASTRO)
+      GROUP BY FACCAO
+      ORDER BY COUNT(*) DESC
+      FETCH FIRST ROW ONLY;
+
+      SELECT COUNT(*) INTO v_qtd_especies_origem
+      FROM ESPECIE
+      WHERE PLANETA_OR = v_planeta_info(v_index).ID_ASTRO;
+
+      DBMS_OUTPUT.PUT_LINE('Planeta: ' || v_planeta_info(v_index).ID_ASTRO);
+      DBMS_OUTPUT.PUT_LINE('Nação dominante atual: ' || v_nacao_dominante);
+      DBMS_OUTPUT.PUT_LINE('Data de início da última dominação: ' || v_data_ini);
+      DBMS_OUTPUT.PUT_LINE('Data de fim da última dominação: ' || v_data_fim);
+      DBMS_OUTPUT.PUT_LINE('Quantidade de comunidades: ' || v_qtd_comunidades);
+      DBMS_OUTPUT.PUT_LINE('Quantidade de espécies: ' || v_q
