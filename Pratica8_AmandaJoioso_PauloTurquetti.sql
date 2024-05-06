195e5435409fd2cc3fb835dea654d0843d3be497
Paulo Turquetti <105894229+PTurquetti@users.noreply.github.com>
2024-05-06 17:53:27 -0300

    Update Pratica8_AmandaJoioso_PauloTurquetti.sql

--- Pratica8_AmandaJoioso_PauloTurquetti.sql
+++ Pratica8_AmandaJoioso_PauloTurquetti.sql
@@ -7,7 +7,10 @@ Paulo Henrique Vedovatto Turquetti - 13750791
 -- QUESTÃO 1 -------------------------------------------------------------------------
 
 
-
+SELECT N.NOME AS NACAO, F.NOME AS FACCAO, D.PLANETA AS PLANETA_DOMINADO FROM 
+FACCAO F JOIN NACAO_FACCAO NF ON F.NOME = NF.FACCAO
+JOIN NACAO N ON NF.NACAO = N.NOME
+JOIN DOMINANCIA D ON D.NACAO = N.NOME;
 
 
 
