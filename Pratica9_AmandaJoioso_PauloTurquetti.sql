4da614ae9181154dad0d73cd0766ff2418b7534d
Paulo Turquetti <105894229+PTurquetti@users.noreply.github.com>
2024-05-15 22:08:11 -0300

    Update Pratica9_AmandaJoioso_PauloTurquetti.sql

--- Pratica9_AmandaJoioso_PauloTurquetti.sql
+++ Pratica9_AmandaJoioso_PauloTurquetti.sql
@@ -62,38 +62,85 @@ END;
 
 -- QUESTÃO 2 --------------------------------------------------------------------------------------------
 
+-- Inserção de dados para testes da questão 2:
+INSERT INTO LIDER (CPI, NOME, CARGO, NACAO, ESPECIE) VALUES ('111.111.111-11', 'Capitã Aria No', 'CIENTISTA', 'Quam quia ad.', 'Quidem quam');
+INSERT INTO LIDER (CPI, NOME, CARGO, NACAO, ESPECIE) VALUES ('222.222.222-22', 'General Zorg', 'COMANDANTE', 'Veniam est.', 'Unde eius at');
+INSERT INTO LIDER (CPI, NOME, CARGO, NACAO, ESPECIE) VALUES ('333.333.333-33', 'Buzz Lightyear', 'OFICIAL', 'Modi porro ut.', 'Iure sunt quas');
+INSERT INTO LIDER (CPI, NOME, CARGO, NACAO, ESPECIE) VALUES ('444.444.444-44', 'Palpatine', 'COMANDANTE', 'Modi porro ut.', 'Iure sunt quas');
+INSERT INTO LIDER (CPI, NOME, CARGO, NACAO, ESPECIE) VALUES ('555.555.555-55', 'Luke Sky', 'COMANDANTE', 'Modi porro ut.', 'Iure sunt quas');
+INSERT INTO LIDER (CPI, NOME, CARGO, NACAO, ESPECIE) VALUES ('666.666.666-66', 'Spock', 'COMANDANTE', 'Modi porro ut.', 'Iure sunt quas');
+INSERT INTO LIDER (CPI, NOME, CARGO, NACAO, ESPECIE) VALUES ('777.777.777-77', 'Groot', 'COMANDANTE', 'Modi porro ut.', 'Iure sunt quas');
+
+INSERT INTO FACCAO (NOME, LIDER, IDEOLOGIA, QTD_NACOES) VALUES ('Prog Celestiais', '111.111.111-11', 'PROGRESSITA', 3);
+INSERT INTO FACCAO (NOME, LIDER, IDEOLOGIA, QTD_NACOES) VALUES ('Cons Cósmicos', '222.222.222-22', 'TRADICIONALISTA', NULL);
+INSERT INTO FACCAO (NOME, LIDER, IDEOLOGIA, QTD_NACOES) VALUES ('Prog e Além', '333.333.333-33', 'PROGRESSITA', NULL);
+
+INSERT INTO NACAO_FACCAO (NACAO, FACCAO) VALUES ('Quam quia ad.', 'Prog Celestiais');
+INSERT INTO NACAO_FACCAO (NACAO, FACCAO) VALUES ('Veniam est.', 'Cons Cósmicos');
+INSERT INTO NACAO_FACCAO (NACAO, FACCAO) VALUES ('Modi porro ut.', 'Prog e Além');
+INSERT INTO NACAO_FACCAO (NACAO, FACCAO) VALUES ('Vel rerum unde.', 'Prog Celestiais');
+INSERT INTO NACAO_FACCAO (NACAO, FACCAO) VALUES ('Ducimus odio.', 'Prog Celestiais');
+INSERT INTO NACAO_FACCAO (NACAO, FACCAO) VALUES ('Veniam est.', 'Prog e Além');
 
 -- FUNÇÃO
-create or replace FUNCTION REMOVE_NACAO_FACCAO(V_FAC NACAO_FACCAO.FACCAO%TYPE, 
-                                                V_NAC NACAO_FACCAO.NACAO%TYPE)
-                                               RETURN NUMBER IS
-                                                
-    V_RESULTADO NUMBER;
-    V_NACAO_FACCAO_TUPLA NACAO_FACCAO%ROWTYPE;
+CREATE OR REPLACE FUNCTION REMOVE_NACAO_FACCAO(
+    V_FAC NACAO_FACCAO.FACCAO%TYPE, 
+    V_NAC NACAO_FACCAO.NACAO%TYPE
+) RETURN NUMBER IS
+    V_RESULTADO NUMBER := 0; -- Inicialize para indicar falha
+BEGIN
+    DELETE FROM NACAO_FACCAO 
+    WHERE FACCAO = V_FAC AND NACAO = V_NAC;
+    
+    -- Verifique se alguma linha foi afetada pela operação de exclusão
+    IF SQL%ROWCOUNT > 0 THEN
+        V_RESULTADO := 1; -- Indica sucesso
+    END IF;
+    
+    RETURN V_RESULTADO;
+END;
 
-    BEGIN
 
-    SELECT * INTO V_NACAO_FACCAO_TUPLA FROM NACAO_FACCAO WHERE NACAO_FACCAO.FACCAO = V_FAC 
-                                                            AND NACAO_FACCAO.NACAO = V_NAC;
+    
+    
+DECLARE
+    V_NACAO_FACCAO NACAO_FACCAO%ROWTYPE;
+    V_NAC NACAO.NOME%TYPE := 'Veniam est.';
+    V_FAC FACCAO.NOME%TYPE := 'Prog e Além';
+BEGIN
+    -- Consulta para obter os detalhes da relação entre a facção e a nação
+    SELECT * INTO V_NACAO_FACCAO 
+    FROM NACAO_FACCAO 
+    WHERE FACCAO = V_FAC AND NACAO = V_NAC;
+    
+    -- Chama a função para remover a relação
+    IF REMOVE_NACAO_FACCAO(V_NACAO_FACCAO.FACCAO, V_NACAO_FACCAO.NACAO) = 1 THEN
+        DBMS_OUTPUT.PUT_LINE('Relação removida com sucesso.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Falha ao remover a relação.');
+    END IF;
+END;
 
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('Nao foi encontrada nenhuma tupla NACAO_FACCAO correspondente');
-            RETURN V_RESULTADO;
 
-        DELETE FROM NACAO_FACCAO WHERE NACAO_FACCAO.FACCAO = V_NACAO_FACCAO_TUPLA.FACCAO 
-                                    AND NACAO_FACCAO.NACAO = V_NACAO_FACCAO_TUPLA.NACAO;
+/*
 
-        DBMS_OUTPUT.PUT_LINE('Tupla de NACAO_FACCAO removida: ');
-        DBMS_OUTPUT.PUT_LINE('Faccao: ' || V_NACAO_FACCAO_TUPLA.FACCAO || '  Nacao: ' || V_NACAO_FACCAO_TUPLA.NACAO);
-        DBMS_OUTPUT.PUT_LINE('-----------------------------------------------------');
+NACAO               FACCAO
+Ducimus odio.	      Prog Celestiais
+Modi porro ut.	    Prog e Além
+Quam quia ad.	      Prog Celestiais
+Vel rerum unde.	    Prog Celestiais
+Veniam est.	        Cons Cósmicos
+Veniam est.	        Prog e Além
 
-        COMMIT;
-        V_RESULTADO := 1;
-        RETURN V_RESULTADO;
-    END;
 
--- CODIGO CHAMANDO A FUNCAO
+NACAO               FACCAO
+Ducimus odio.	      Prog Celestiais
+Modi porro ut.	    Prog e Além
+Quam quia ad.	      Prog Celestiais
+Vel rerum unde.    	Prog Celestiais
+Veniam est.	        Cons Cósmicos
+
+*/
 
 
 -- QUESTÃO 3 --------------------------------------------------------------------------------------
