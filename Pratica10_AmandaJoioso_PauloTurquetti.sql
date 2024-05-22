7d8647cf7c067d771e3bf83941a57f1bc05aef46
Paulo Turquetti <105894229+PTurquetti@users.noreply.github.com>
2024-05-22 20:31:29 -0300

    Update Pratica10_AmandaJoioso_PauloTurquetti.sql

--- Pratica10_AmandaJoioso_PauloTurquetti.sql
+++ Pratica10_AmandaJoioso_PauloTurquetti.sql
@@ -1,1 +1,19 @@
+/* Pratica 9
+
+Amanda Valukas Breviglieri Joioso - 4818232
+Paulo Henrique Vedovatto Turquetti - 13750791
+*/
+
+
+-- QUESTÃO 1 --------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
 
