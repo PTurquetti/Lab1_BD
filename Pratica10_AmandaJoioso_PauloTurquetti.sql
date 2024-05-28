0e895ea86663c3fb4bf25230725bef7a26c578b5
Paulo Turquetti <105894229+PTurquetti@users.noreply.github.com>
2024-05-28 12:09:10 -0300

    Update Pratica10_AmandaJoioso_PauloTurquetti.sql

--- Pratica10_AmandaJoioso_PauloTurquetti.sql
+++ Pratica10_AmandaJoioso_PauloTurquetti.sql
@@ -7,38 +7,61 @@ Paulo Henrique Vedovatto Turquetti - 13750791
 
 -- QUESTÃO 1 --------------------------------------------------------------------------------------
 
--- a)
-
--- Buscas para apoio
-SELECT * FROM FEDERACAO WHERE NOME = 'NOVA FED';
-SELECT * FROM FEDERACAO F JOIN NACAO N ON F.NOME = N.FEDERACAO WHERE F.NOME = 'NOVA FED';
-
+-- a) Federacoes sem faccao
 
 CREATE OR REPLACE TRIGGER FEDERACAO_SEM_NACAO
 BEFORE INSERT ON FEDERACAO
 BEGIN
     
-    DELETE FROM FEDERACAO WHERE NOME NOT IN (SELECT NOME FROM FEDERACAO 
-                                        MINUS 
-                                        SELECT F.NOME FROM FEDERACAO F JOIN NACAO N ON F.NOME = N.FEDERACAO);
+    DELETE FROM FEDERACAO WHERE NOME NOT IN (SELECT F.NOME FROM FEDERACAO F JOIN NACAO N ON F.NOME = N.FEDERACAO);
 
 END;
 
-/* ANALISANDO RESULTADOS
-
-Obs: Quando vamos inserir uma nova federação, ela não terá nenhuma nação associada devido à implementacao da FK em nacao.
+/*   ANALISANDO 
+    Quando vamos inserir uma nova federação, ela não terá nenhuma nação associada devido à implementacao da FK em nacao.
 com isso, mesmo se logo após da inserção da dfederacao fizermos a inserçao da nova nacao associada a ela, a existencia de um
 trigger que apaga federacoes sem nacao associada executada após cada inserção de uma federação nao permitiria que federeções novas
 fossem inseridas. 
-Sendo assim, o que podemos fazer é, após um insert, update ou delete de uma nação, verificar se alguma federação ficou sem nação associada.
-Isso permite a inserção de uma nova federação seguida da inserção sua nova nacao, além de garantir que a federação que perder suas
-nações devido à updates ou deletes será excluída
-*/
-INSERT INTO FEDERACAO (NOME, DATA_FUND) VALUES ('NOVA FED', SYSDATE);
-INSERT INTO FEDERCAO (NOME, DATA_FUND) VALUES ('MAIS NOVA', SYSDATE);
 
--- DELETE FROM FEDERACAO WHERE NOME = 'NOVA FED';
--- DELETE FROM FEDERACAO WHERE NOME = 'MAIS NOVA';
+    Sendo assim, o que podemos fazer é verificar se alguma federação não possui nação associada antes de cada inserção em FEDERACAO.
+Isso irá permitir a existência de uma federação sem nacao a cada inserção,, permitindo que seja inserida a nação associada a ele.
+
+    No entanto. essa solução enfrenta problemas. Caso ocorram updates ou deletes na tabela de nacao que deixem mais federações sem nacao,
+elas continuarão existindo até que uma nova inserção de federação seja realizada, possibilitando múltiplas fererações sem nação simultaneamente.
+
+Vamos agora testar o funcionamento do trigger:
+*/
+
+-- Inserindo a primeira federação
+INSERT INTO FEDERACAO (NOME, DATA_FUND) VALUES ('NOVA FED', SYSDATE);
+
+-- Consulta que mostra federacoes sem nacao associada
+SELECT * FROM FEDERACAO WHERE NOME NOT IN (SELECT F.NOME FROM FEDERACAO F JOIN NACAO N ON F.NOME = N.FEDERACAO);
+-- NOVA FED	 28/05/24
+
+-- Inserindo uma nova federacao (ativacao do trigger)
+INSERT INTO FEDERACAO (NOME, DATA_FUND) VALUES ('MAIS NOVA', SYSDATE);
+
+-- Consulta que mostra federacoes sem nacao associada
+SELECT * FROM FEDERACAO WHERE NOME NOT IN (SELECT F.NOME FROM FEDERACAO F JOIN NACAO N ON F.NOME = N.FEDERACAO);
+-- MAIS NOVA	28/05/24
+
+-- Consulta mostrando as federacoes teste que estamos utilizando
+SELECT * FROM FEDERACAO WHERE NOME IN ('NOVA FED', 'MAIS NOVA');
+--MAIS NOVA 	28/05/24
+
+/* Portanto, podemos notar que antes de inserirmos MAIS NOVA, a federacao NOVA FED pode existir sem nacao associada.
+No entando, NOVA FED foi deletada quando a inserção de MAIS NOVA ativou o trigger, tornando MAIS NOVA a unica federacao sem nacao
+( lembrando que deletes e updates en NACAO podem deixar mais federacoes sem nacao enquanto não houver outra insercao em federacao.
+
+*/
+-- Apagando dados de teste
+DELETE FROM FEDERACAO WHERE NOME = 'MAIS NOVA';
+
+
+
+
+-- b) O	líder de uma facção	deve estar associado a uma nação em	que	a facção está presente.	
 
 
 
